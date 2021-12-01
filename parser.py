9c8d384411c76cc7bf3eedc7e8851dbb6c667193
Thomas Marwitz <thomasmarwitz3@gmail.com>
2021-12-01 18:03:04 +0100

    parsing javadoc

---
+++ parser.py
@@ -0,0 +1,79 @@
+from collections import namedtuple
+import os
+import re
+
+class ReactComponent:
+    #name, propName, props, state, callbacks, docstring
+    def __init__(self, name, propName, file):
+        self.name = name
+        self.file = file
+        self.propName = propName
+        self.content = open(self.file, "r", encoding="utf8").read()
+        self.docstring = "<<empty>>"
+        self.props = []
+        self.state = []
+        self.callbacks = []
+        self.children = []
+
+    def parseDocstring(self):
+        index = self.content.index(self.name) # fetch first javadoc before comment
+        doc_index = self.content[:index].rfind("/**")
+        if doc_index == -1:
+            return
+        end_index = self.content[doc_index:index].find("*/") + doc_index # no cut of
+        
+        javadoc = self.content[doc_index:end_index].split("\n")
+        # transform javadoc
+        trailing_stars = [line.strip() for line in javadoc[1:] if line.strip() != "*"]
+        plain_text = [line[1:].strip() for line in trailing_stars if line.startswith("*")]
+        self.docstring = "\n".join(plain_text)
+
+        
+
+
+    def __str__(self):
+        return f"{self.name}, {self.propName}, {self.file}"
+
+
+BASE_DIR = "../pse-prototype/src"
+
+File = namedtuple("File", ("name", "path"))
+
+def get_component_info(lines):
+    for line in lines:
+        mo = re.search(".*function\s+(\w+)\s*\((\w*)\)\s*.*", line)
+        if mo:
+            if mo.group(1)[0].isupper(): # assumes that only react components start with Uppercase
+                return (mo.group(1), mo.group(2))
+
+def parse_file(fileObj):
+    lines = open(fileObj.path, "r", encoding="utf-8").readlines()
+    data = get_component_info(lines)
+    if not data:
+        return
+    react_comp = ReactComponent(name=data[0], propName=data[0], file=fileObj.path)
+    return react_comp
+    
+   
+                
+
+
+
+
+all_files = []
+for path, subdirs, files in os.walk(BASE_DIR):
+    for name in files:
+        f = File(name, os.path.join(path, name))
+        all_files.append(f)
+
+files = [f for f in all_files if f.name.endswith(".js")]
+
+
+components = [parse_file(f) for f in files]
+componentNames = [comp.name for comp in components if comp]
+print(componentNames)
+
+comp = components[0]
+comp.parseDocstring()
+print(comp.docstring)
+
